f76000cea0f2532d3e031c652a0c3e8f9a8501d0
Gabriel Luz <gfluz94@gmail.com>
2020-08-28 16:10:07 -0300

    adding files to enable docker  + buil repository

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,30 @@
 import os
+import requests
+import json
+import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
-
 if __name__ == '__main__':
-    pass
+    data = json.loads('{"name":"{{cookiecutter.project_slug}}","description":"{{cookiecutter.project_description|e}}"}')
+    data = json.dumps(data)
+    r = requests.post('https://api.github.com/orgs/{{cookiecutter.github_organization}}/repos',
+                    data = data,
+                    headers={'Authorization': 'token {{cookiecutter.github_token}}'}
+                    )
+    if r.status_code != 201:
+        print('ERROR: unable to create a new GitHub repository!')
+    else:
+        print('INFO: GitHub repository successfully created.')
+
+        # on initialise le dépôt GitHub
+        subprocess.call(['git', 'init'])
+        # on commit
+        subprocess.call(['git', 'add', '*'])
+        subprocess.call(['git', 'commit', '-m', '"first commit"'])
+        # on push
+        subprocess.call(['git', 'remote', 'add', 'origin', 'git@github.com:{{cookiecutter.github_organization}}/{{cookiecutter.project_slug}}.git'])
+        subprocess.call(['git', 'push', '-u', 'origin', 'master'])
